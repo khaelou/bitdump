0fe2ed2974318cc9e248773396ef0aa0f58d6167
My-khael Pierce <mykhpl@gmail.com>
2022-08-02 03:56:09 -0600

    Remove whitespace

--- factory/factory.go
+++ factory/factory.go
@@ -8,7 +8,6 @@ import (
 var SproutedJobs []worker.Job
 
 func CreateJobs(amount int) []worker.Job {
-
 	for i := 0; i < amount; i++ {
 		newJob := worker.Job{ID: i, Focus: "Winning Numbers", ExecFunc: macro.TicketPool}
 
